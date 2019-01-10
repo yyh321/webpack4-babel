52e34ddd09f74586c934062c2a9bd7924be50193
yyh123 <yuyunhao1@126.com>
2019-01-10 21:48:54 +0800

    webpack4 && babel

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import './index.scss'
+import './index.css'
+
+let HelloWorld = () => {
+    return <h1 > Hello World!! < /h1>
+}
+
+ReactDOM.render( < HelloWorld / > , document.getElementById('root'))
